3ae7bc3e5ddefce0b7c9e0055e7cbfdada7c8a6e
Nicolas <sabrenicolas59@gmail.com>
2020-02-08 19:59:23 +0100

    calendar alert

--- app/src/main/java/com/example/mylife/Calendar.java
+++ app/src/main/java/com/example/mylife/Calendar.java
@@ -2,29 +2,31 @@ package com.example.mylife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
 import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 
+import java.util.Date;
 
-public class Calendar extends AppCompatActivity{
 
-    public static final String RESULT = "result";
-    public static final String EVENT = "event";
-    private static final int ADD_NOTE = 44;
+public class Calendar extends AppCompatActivity {
 
+    private static final String CAT = "CALENDAR";
     private CalendarView mCalendarView;
+    private String dateClicked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
-
+        mCalendarView = findViewById(R.id.calendarView);
 
 
         mCalendarView.setOnDayClickListener(new OnDayClickListener() {
@@ -32,10 +34,17 @@ public class Calendar extends AppCompatActivity{
             @Override
             public void onDayClick(EventDay eventDay) {
              java.util.Calendar clicked = eventDay.getCalendar();
-                System.out.println("ok click");
-                System.out.println(clicked.getTime());
+                dateClicked = clicked.getTime().toString();
+                alerter(dateClicked);
             }
         });
+    }
+
+    private void alerter(String s) {
+        Log.i(CAT, s);
+        Toast myToast = Toast.makeText(this, s, Toast.LENGTH_SHORT);
+        myToast.show();
+
     }
 
 }
