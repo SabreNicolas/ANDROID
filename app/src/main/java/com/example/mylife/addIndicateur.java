0e321f9534065dae0de78bff6650750c738d302c
Nicolas <sabrenicolas59@gmail.com>
2020-04-02 11:33:55 +0200

    add indicateur V1

--- app/src/main/java/com/example/mylife/addIndicateur.java
+++ app/src/main/java/com/example/mylife/addIndicateur.java
@@ -2,31 +2,38 @@ package com.example.mylife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import classBDD.Indicateur;
 import classBDD.User;
 
-public class addIndicateur extends AppCompatActivity implements View.OnClickListener {
+public class addIndicateur extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     private static final String CAT = "IME";
     private EditText editTextName;
     private Spinner selectType;
     private Button btnTerminer;
-    private TextView tv;
     private Spinner nbValues;
-    private ScrollView ScrollView;
+    private LinearLayout zoneAddNbValues;
+    private LinearLayout zoneAddValues;
+    private Integer nbVal = 0;
     private User user;
     private Indicateur indicateur;
     private String valueInit;
@@ -42,11 +49,10 @@ public class addIndicateur extends AppCompatActivity implements View.OnClickList
 
         editTextName = findViewById(R.id.Indicateur);
         editTextName.setOnClickListener(this);
-        tv = findViewById(R.id.nbValues);
-        ScrollView = findViewById(R.id.ScrollViewValues);
-        nbValues = findViewById(R.id.values);
         selectType = findViewById(R.id.SpinnerTypeIndicateur);
-
+        selectType.setOnItemSelectedListener(this);
+        zoneAddNbValues = (LinearLayout) findViewById(R.id.dynamicNbValues);
+        zoneAddValues = (LinearLayout) findViewById(R.id.dynamicValues);
         btnTerminer = findViewById(R.id.buttonTerminerAddIndicateur);
         btnTerminer.setOnClickListener(this);
 
@@ -59,76 +65,6 @@ public class addIndicateur extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    //TODO pas mettre en dur le 2e select, le générer à la volée
-    @Override
-    public void onResume() {
-        super.onResume();
-        selectType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-        {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
-            {
-                String selectedItem = parent.getItemAtPosition(position).toString();
-                if(selectedItem.equals("Case à cocher") || selectedItem.equals("Menu déroulant"))
-                {
-                    nbValues.setVisibility(View.VISIBLE);
-                    tv.setVisibility(View.VISIBLE);
-                }
-                else {
-                    nbValues.setVisibility(View.INVISIBLE);
-                    tv.setVisibility(View.INVISIBLE);
-                }
-            } // to close the onItemSelected
-            public void onNothingSelected(AdapterView<?> parent)
-            {
-                System.out.println("rien n'est selectionné ...");
-                nbValues.setVisibility(View.INVISIBLE);
-                tv.setVisibility(View.INVISIBLE);
-            }
-        });
-
-        nbValues.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-        {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
-            {
-                System.out.println("click");
-                String selectedItem = parent.getItemAtPosition(position).toString();
-                Integer nbVal = Integer.parseInt(selectedItem);
-                LinearLayout ll = (LinearLayout) findViewById(R.id.dynamic);
-
-                // TODO car 2 est selectionné de base et supprimer les lignes en trop
-                // exemple : 5 puis 2, il ajoute 2 aux 5 déjà présentes => vider d'abord la scroll view ?
-                /*if(nbVal == 2){
-                    sv.setVisibility(View.INVISIBLE);
-                    for (int i=1; i<=nbVal; i++){
-                        EditText edit = new EditText(getApplicationContext());
-                        edit.setId(R.id.values+i);
-                        ll.addView(edit);
-                        EditText editUpdate = findViewById(R.id.values+i);
-                        editUpdate.setText("test"+i);
-                    }
-                }*/
-
-
-                ScrollView.setVisibility(View.VISIBLE);
-                for (int i=1; i<=nbVal; i++){
-                    EditText edit = new EditText(getApplicationContext());
-                    edit.setId(R.id.values+i);
-                    ll.addView(edit);
-                    EditText editUpdate = findViewById(R.id.values+i);
-                    editUpdate.setText("test"+i);
-                }
-
-                //else sv.setVisibility(View.INVISIBLE);
-            } // to close the onItemSelected
-            public void onNothingSelected(AdapterView<?> parent)
-            {
-                System.out.println("rien n'est selectionné ...");
-                ScrollView.setVisibility(View.INVISIBLE);
-            }
-        });
-    }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // affichage des boutons du menu
@@ -137,15 +73,137 @@ public class addIndicateur extends AppCompatActivity implements View.OnClickList
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id) {
+            case R.id.action_historique :
+                // affiche de l'activité historique
+                Intent versPrefs = new Intent(this, Calendar.class);
+                startActivity(versPrefs);
+                break;
+            case R.id.addIndicateur :
+                // affiche de l'activité add Indicateur
+                Intent versaddIndicateur = new Intent(this, addIndicateur.class);
+                startActivity(versaddIndicateur);
+                break;
+            case R.id.listIndicateur :
+                // affiche la liste des indicateurs de l'user
+                Intent listIndicateurs= new Intent(this,ListIndicateurs.class);
+                startActivity(listIndicateurs);
+                break;
+            case R.id.addEspace :
+                // affiche de l'activité add Espace
+                Intent versaddEspace = new Intent(this, addEspace.class);
+                startActivity(versaddEspace);
+                break;
+            case R.id.listEspace:
+                // affiche la liste des indicateurs de l'user
+                Intent listEspaces= new Intent(this,ListEspaces.class);
+                startActivity(listEspaces);
+                break;
+            case R.id.action_settings : break;
+            case R.id.action_account : break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void alerter(String s) {
+        Log.i(CAT, s);
+        Toast myToast = Toast.makeText(this, s, Toast.LENGTH_SHORT);
+        myToast.show();
+
+    }
+
+    @Override
     public void onClick(View v) {
+        // méthode appellé lors du click sur editPseudo ou idbutton
         switch (v.getId()) {
-            case R.id.SpinnerTypeIndicateur:
-                //String type = selectType;
+            case R.id.buttonTerminerAddIndicateur:
+                String name = editTextName.getText().toString();
+                if(name.isEmpty()){
+                    alerter("Saisir un nom pour l'espace");
+                    return;
+                }
+
+                String valeurInit ="";
+                for (int i=1; i<=nbVal; i++){
+                    EditText editUpdate = findViewById(R.id.dynamicValues+i);
+                    String value = editUpdate.getText().toString();
+                    if(value.isEmpty()){
+                        alerter("Il y a des valeurs vides");
+                        return;
+                    }
+                    else{
+                        valeurInit = valeurInit + editUpdate.getText() + ";";
+                    }
+                }
+
+                System.out.println("ajout d'un indicateur avec valeur init = " +valeurInit);
+                alerter(valeurInit);
+
+                /*addIndicateur.JSONAsyncTask jsAT = new addIndicateur().JSONAsyncTask();
+
+                if(this.indicateur == null){
+                    this.httpType = "POST";
+                    this.reqBody = null;
+                    jsAT.execute("/espaces/?nomEspace=" + name+"&idUser="+ user.getId());
+                }
+                else {
+                    this.httpType = "PUT";
+                    reqBody = new JSONObject();
+                    try {
+                        reqBody.put("nomEspace", name);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    jsAT.execute("/espaces/"+espace.getId());
+                }*/
 
                 break;
-            default:
-                System.out.println("cas non prévu");
+            default:    alerter("cas non prévu");
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // An item was selected. You can retrieve the selected item using
+        String selectedItem = parent.getItemAtPosition(position).toString();
+
+        if(selectedItem.equals("Case à cocher") || selectedItem.equals("Menu déroulant") || selectedItem.equals("Champ de saisie") || selectedItem.equals("Oui ou Non") || selectedItem.equals("Curseur")){
+            zoneAddNbValues.removeAllViews();
+
+            if(selectedItem.equals("Case à cocher") || selectedItem.equals("Menu déroulant"))
+            {
+                TextView libelleNbValues = new TextView(getApplicationContext());
+                libelleNbValues.setText("Nombre de valeurs possibles :");
+                libelleNbValues.setTextSize(25);
+                zoneAddNbValues.addView(libelleNbValues);
+                nbValues = new Spinner(getApplicationContext());
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.listNbValues, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                nbValues.setAdapter(adapter);
+                nbValues.setOnItemSelectedListener(this);
+                zoneAddNbValues.addView(nbValues);
+            }
+            else {
+                zoneAddValues.removeAllViews();
+            }
+        }
+        else{
+            zoneAddValues.removeAllViews();
+            nbVal = Integer.parseInt(selectedItem);
+            for (int i=1; i<=nbVal; i++){
+                EditText edit = new EditText(getApplicationContext());
+                edit.setId(R.id.dynamicValues+i);
+                zoneAddValues.addView(edit);
+            }
         }
 
     }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
 }
